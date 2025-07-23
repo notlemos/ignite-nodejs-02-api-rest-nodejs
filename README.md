2c17eab06c07242cda77a684d70602221a3d994a
notlemos <caiolemossfc123@hotmail.com>
2025-07-23 15:09:21 -0300

    Listagem de Transações

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 - [x] O usuário deve poder criar uma nova transação;
 - [ ] O usuário deve poder obter um resumo da sua conta;
-- [ ] O usuário deve poder listar todas as transações que já ocorreram;
-- [ ] O usuário deve poder visualizar uma transação única;
+- [x] O usuário deve poder listar todas as transações que já ocorreram;
+- [x] O usuário deve poder visualizar uma transação única;
 
 # RN 
 
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,25 @@ import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     // { title, amount, type; credit or debit}
     const createTransactionBodySchema = z.object({
